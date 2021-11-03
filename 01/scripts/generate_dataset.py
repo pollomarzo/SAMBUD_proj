1fcc5f4e8e843df5a2c0f1b061399c55e4242251
Paolo Marzolo <pollo.marzo@gmail.com>
2021-11-03 19:21:22 +0100

    included GUI boiler + edited generation

--- 01/scripts/generate_dataset.py
+++ 01/scripts/generate_dataset.py
@@ -15,9 +15,41 @@ people = [
         'first_name': names.get_first_name(),
         'last_name': names.get_last_name(),
         'positive': random.choice([True, False]),
-        'last_confirm': min_datetime + (max_datetime - min_datetime) * random.random(),
+        # brith, phone_number, email
+        'birth': '01/01/1990',
+        'phone_number': '000',
+        'email': 'mail@example.com',
+        'last_confirm': random.choice([None, min_datetime + (max_datetime - min_datetime) * random.random()]),
     }) for i in range(SIZE_DATASET)
 ]
+
+medical_records = [
+    ({
+        'code': i,
+        'covid_vaccinated': random.choice([True, False]),
+        'risky_subject': random.choice([True, False]),
+        'health_status': random.choice(["bad", "average", "good"]),
+    }) for i in range(SIZE_DATASET)
+]
+
+medical_vaccines = []
+
+for i in medical_records:
+    if (i.covid_vaccinated):
+        medical_vaccines.append({
+            'code': i.code,
+            'date': min_datetime + (max_datetime - min_datetime) * random.random(),
+            'type': random.choice(["pfizer", "moderna", "astrazeneca"])
+        })
+
+covid_tests = []
+for i in people:
+    if (i.last_confirm != None):
+        covid_tests.append({
+            'code': i.code,
+            'date': i.last_confirm,
+            'result': i.positive,
+        })
 
 
 print(people)
