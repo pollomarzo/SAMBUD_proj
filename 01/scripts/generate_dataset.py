b8bb81bc7d6ded20c0929364bc8514b6ad300806
filippomanzardo <87145535+filippomanzardo@users.noreply.github.com>
2021-11-03 23:37:36 +0100

    Bug Fix

--- 01/scripts/generate_dataset.py
+++ 01/scripts/generate_dataset.py
@@ -1,6 +1,5 @@
 import names
 import random
-<<<<<<< HEAD
 import numpy as np
 import csv
 
@@ -177,61 +176,3 @@ if __name__ == "__main__":
     saveCSV(covid_vaccines, 'covid_vaccines.csv')
     saveCSV(covid_tests, 'covid_tests.csv')
     saveCSV(contacts, 'contacts.csv')
-
-
-
-=======
-
-from datetime import datetime, timedelta
-
-
-SIZE_DATASET = 10
-
-min_datetime = datetime(2018, 1, 1, 0, 0, 0)
-max_datetime = datetime.now()
-
-people = [
-    ({
-        'code': i,
-        'first_name': names.get_first_name(),
-        'last_name': names.get_last_name(),
-        'positive': random.choice([True, False]),
-        # brith, phone_number, email
-        'birth': '01/01/1990',
-        'phone_number': '000',
-        'email': 'mail@example.com',
-        'last_confirm': random.choice([None, min_datetime + (max_datetime - min_datetime) * random.random()]),
-    }) for i in range(SIZE_DATASET)
-]
-
-medical_records = [
-    ({
-        'code': i,
-        'covid_vaccinated': random.choice([True, False]),
-        'risky_subject': random.choice([True, False]),
-        'health_status': random.choice(["bad", "average", "good"]),
-    }) for i in range(SIZE_DATASET)
-]
-
-medical_vaccines = []
-
-for i in medical_records:
-    if (i.covid_vaccinated):
-        medical_vaccines.append({
-            'code': i.code,
-            'date': min_datetime + (max_datetime - min_datetime) * random.random(),
-            'type': random.choice(["pfizer", "moderna", "astrazeneca"])
-        })
-
-covid_tests = []
-for i in people:
-    if (i.last_confirm != None):
-        covid_tests.append({
-            'code': i.code,
-            'date': i.last_confirm,
-            'result': i.positive,
-        })
-
-
-print(people)
->>>>>>> 1fcc5f4e8e843df5a2c0f1b061399c55e4242251
