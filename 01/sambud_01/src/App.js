4ffe97cd3d1988816673b6d076dc433e7eab6897
Paolo Marzolo <pollo.marzo@gmail.com>
2021-11-11 07:40:18 +0100

    started working on webapp

--- 01/sambud_01/src/App.js
+++ 01/sambud_01/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { Button } from '@mui/material';
 
 function App() {
+  const [message, setMessage] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        {message}
+      </p>
+      <Button onClick={ }>
+        Carica dati
+      </Button>
     </div>
   );
 }
--- 01/sambud_01/src/index.js
+++ 01/sambud_01/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Neo4jProvider, createDriver } from 'use-neo4j'
+
+const driver = createDriver('neo4j+s://cbdc369d.databases.neo4j.io', 'myauradb.neo4j.io', 7687, 'neo4j', 'mYYGw4dV-v7GLAuwWUuwfhqCJKUs_zSR4ytR-xf-MRk')
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Neo4jProvider
+      driver={driver}
+    >
+      <App />
+    </Neo4jProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
