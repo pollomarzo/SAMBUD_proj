a8c82b483f4743e5f334a8e525360dd8e7c5f524
Paolo Marzolo <pollo.marzo@gmail.com>
2021-11-14 23:56:23 +0100

    removed useless defaults

--- 01/sambud_01/src/App.js
+++ 01/sambud_01/src/App.js
@@ -34,30 +34,7 @@ import {
 
 function App({ driver }) {
   const [message, setMessage] = useState("");
-  const [elements, setElements] = useState([
-    {
-      id: "1",
-      type: "input", // input node
-      data: { label: "Input Node" },
-      position: { x: 250, y: 25 },
-    },
-    // default node
-    {
-      id: "2",
-      // you can also pass a React component as a label
-      data: { label: <div>Default Node</div> },
-      position: { x: 100, y: 125 },
-    },
-    {
-      id: "3",
-      type: "output", // output node
-      data: { label: "Output Node" },
-      position: { x: 250, y: 250 },
-    },
-    // animated edge
-    { id: "e1-2", source: "1", target: "2", animated: true },
-    { id: "e2-3", source: "2", target: "3" },
-  ]);
+  const [elements, setElements] = useState([]);
   const [selected, setSelected] = useState(undefined);
   const [result, setResult] = useState(undefined);
 
@@ -96,9 +73,9 @@ function App({ driver }) {
                 <ResponsiveNeoGraph
                   driver={driver}
                   containerId={"id0"}
-                  neo4jUri={"bolt://localhost:7687"}
+                  neo4jUri={NEO4J_HOST_URI}
                   neo4jUser={NEO4J_USER}
-                  neo4jPassword={"mamoud"}
+                  neo4jPassword={NEO4J_PASSWORD}
                   height={"100%"}
                   width={"100%"}
                   cypher={result.cypher}
